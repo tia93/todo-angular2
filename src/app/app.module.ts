3fd204b8e05aa1c287cdca141b9968b0926fe64e
tia93 <103486517+tia93@users.noreply.github.com>
2022-06-28 13:29:48 +0200

    fine 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
-<<<<<<< HEAD
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
@@ -12,12 +11,6 @@ import {MatCardModule} from '@angular/material/card';
 
 
 
-=======
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
->>>>>>> 7d4184a389df47402800a1438764f867cc21bded
 
 import { AppComponent } from './app.component';
 import { ListComponent } from './components/list/list.component';
@@ -28,10 +21,7 @@ import { TagsStringPipe } from './pipes/tags-string/tags-string.pipe';
 import { PrioritySortPipe } from './pipes/priority-sort/priority-sort.pipe';
 import { EditComponent } from './components/edit/edit.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-<<<<<<< HEAD
 // import { MatCommonModule } from '@angular/material/core';
-=======
->>>>>>> 7d4184a389df47402800a1438764f867cc21bded
 
 
 
@@ -56,12 +46,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
-<<<<<<< HEAD
     MatCardModule,
 
-=======
-    MatCardModule
->>>>>>> 7d4184a389df47402800a1438764f867cc21bded
   ],
   providers: [],
   bootstrap: [AppComponent]
